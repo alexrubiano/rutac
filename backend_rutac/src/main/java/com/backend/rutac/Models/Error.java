fb61346cc5db3e969dc70d164186312e48596384
Alexander Rubiano <81596298+alexrubiano@users.noreply.github.com>
2022-12-07 19:31:52 -0500

    Delete backend_rutac directory

--- backend_rutac/src/main/java/com/backend/rutac/Models/Error.java
+++
@@ -1,9 +0,0 @@
-package com.backend.rutac.Models;
-
-import lombok.Data;
-
-@Data
-public class Error {
-    private String Field;
-    private String Message;
-}
--- backend_rutac/src/main/java/com/backend/rutac/Models/NotEmpty.java
+++
@@ -1,6 +0,0 @@
-package com.backend.rutac.Models;
-
-public @interface NotEmpty {
-
-}
-
